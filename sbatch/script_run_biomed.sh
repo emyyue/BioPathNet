f2807fb25d405c2aca26e8aae81e51a20e59d4c5
emyyue <emyyue>
2023-05-09 17:53:00 +0200

    update gridsearch run

--- sbatch/script_run_biomed.sh
+++ sbatch/script_run_biomed.sh
@@ -3,5 +3,5 @@
 
 for i in {1..7}
 do
-    sbatch script_run_biomed.sh config/knowledge_graph/hyperparam/biomed_joint_${i}.yaml
+    sbatch --output=./slurm_out/job_output${i}.txt --error=./slurm_out/job_error${i}.txt run_biomed.sh ../config/knowledge_graph/hyperparam/biomed_joint_${i}.yaml
 done

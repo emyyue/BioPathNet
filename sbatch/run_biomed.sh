8c4d3debf39702289800cc144fd8df7d86a280cc
emyyue <emyyue>
2023-05-09 19:23:09 +0200

    change dir and slurm compatible

--- sbatch/run_biomed.sh
+++ sbatch/run_biomed.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 #SBATCH --job-name=nbfnet_biomed
-#SBATCH --output=./slurm_out/job_output.txt
-#SBATCH --error=./slurm_out/job_error.txt
+#SBATCH --output=./slurm_out/job_output"$1".txt
+#SBATCH --error=./slurm_out/job_error"$1".txt
 #SBATCH --time=12:00:00
-#SBATCH --mem=24Gb
+#SBATCH --mem=64Gb
 #SBATCH -c 4
 #SBATCH --gres=gpu:1
-#SBATCH --partition=main
+#SBATCH -p gpu_p
+#SBATCH --qos=gpu
 
-# TODO: change to how to activate environment
-module load python
-source ~/allvirtualenvs/nbfnet/bin/activate 
-module load cuda/11.1
-cd /home/mila/y/yue.hu/github/NBFNet
+CONDA_DIR=/home/icb/yue.hu/proj_genefun/conda-env/miniconda3
+eval "$($CONDA_DIR/bin/conda shell.bash hook)"
 
+conda activate env_re_nbfnet
 
-python script/run.py -c ${1} --gpus [0] --version v1
+export PATH=/usr/local/cuda-11.8/bin:$PATH
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-11.8/lib64
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-11.8/targets/x86_64-linux/lib
+
+python ../script/run.py -c ${1} --gpus [0] --version v1

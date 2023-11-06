edbbac68d2f8425bc491f0f063219c8480b491ad
emyyue <emyyue>
2023-11-06 15:48:20 +0100

    Add calculation of AUPRC/AUROC for one pos to one neg scenario

--- script/txgnn_evaluate.py
+++ script/txgnn_evaluate.py
@@ -110,7 +110,7 @@ def pred_to_dataframe(pred, dataset, entity_vocab, relation_vocab):
         # prob = sigmoid(pred[:, j, :])
         prob = (pred[:, j, :])
         prob = prob.flatten().cpu().numpy()
-        temp = {'query_node': np.repeat([dataset.entity_vocab[i] for i in [x.numpy()[j] for x in solver.test_set]], len(nodes)),
+        df_dict = {'query_node': np.repeat([dataset.entity_vocab[i] for i in [x.numpy()[j] for x in solver.test_set]], len(nodes)),
                    'query_relation': np.repeat(testset_relation, len(nodes)),
                    'reverse': j,
                    'pred_node': np.tile(nodes, len(testset_relation)),
@@ -118,13 +118,13 @@ def pred_to_dataframe(pred, dataset, entity_vocab, relation_vocab):
                    'probability':prob.tolist()}
          
         # mask out unwanted
-        mymask = temp['pred_node_type'] == 4
-        df_dict = {'query_node': temp['query_node'][mymask],
-                   'query_relation': temp['query_relation'][mymask],
-                   'reverse': j,
-                   'pred_node':  temp['pred_node'][mymask],
-                   'pred_node_type':  temp['pred_node_type'][mymask],
-                   'probability': prob[mymask]}
+        # mymask = temp['pred_node_type'] == 4
+        # df_dict = {'query_node': temp['query_node'][mymask],
+        #            'query_relation': temp['query_relation'][mymask],
+        #            'reverse': j,
+        #            'pred_node':  temp['pred_node'][mymask],
+        #            'pred_node_type':  temp['pred_node_type'][mymask],
+        #            'probability': prob[mymask]}
            
         dflist.append(df_dict)
     df = pd.concat([pd.DataFrame(dflist[0]),pd.DataFrame(dflist[1])])
@@ -137,6 +137,69 @@ def pred_to_dataframe(pred, dataset, entity_vocab, relation_vocab):
 def sig(x):
  return 1/(1 + np.exp(-x))
 
+
+def get_auprc_oneonone(mydir, cfg, df):
+    def get_index_of_evalG(rel="contraindication", type="pos", rev=1):
+        if rev == 1:
+            rel = 'rev_' + rel 
+        print("### ", rel, " ###")
+        df_g = g.loc[(g.rel == rel) & (g.type==type)]
+        x = df_g.merge(diseases, left_on="disease", right_on="idx",how="left")
+        y = x.merge(drugs, left_on="drug", right_on="idx",how="left")   
+        y.rename(columns={"index_x": "index_disease", "index_y": "index_drug"}, inplace=True)
+        #print("Number of missing index or idx: ", y.isnull().sum().sum())
+        y['tomatch'] = (y['index_drug'].astype(str) + '_' +  y['index_disease'].astype(str))
+        return y
+
+    def get_preds_for_evalG(df_index, rel="contraindication", rev=1):
+        df_pred = df.loc[(df.query_relation==rel) & (df.reverse==rev)]
+        df_m = df_index.merge(df_pred[['probability','tomatch']], on="tomatch", how="left")
+        print("Percentage of missing probabilities replaced by zero: ", df_m.isnull().sum().sum(), "/", len(df_index))
+        df_m["probability"] = df_m["probability"].fillna(0)
+        return df_m
+
+
+    def get_probs(rel="contraindication", type="pos", rev=1):
+        df_index = get_index_of_evalG(rel=rel, type=type, rev=rev)
+        df_m = get_preds_for_evalG(df_index, rel=rel, rev=rev)
+        return torch.tensor(df_m['probability'].values)
+
+    g = pd.read_csv(os.path.join(mydir, "all_data.csv", sep=","))
+    test = pd.read_csv(os.path.join(cfg.dataset.path, "../test.csv"), sep=",")
+    test['x_idx'] = test['x_idx'].astype(int)
+    test['y_idx'] = test['y_idx'].astype(int)
+    # get mapping for index and idx
+    test_dd1 = test.loc[(test.x_type=="disease") & (test.y_type=="drug")]
+    test_dd2 = test.loc[(test.x_type=="drug") & (test.y_type=="disease")]
+
+    diseases = pd.concat([test_dd1[['y_index', 'x_idx']].rename(columns={"y_index": "index", "x_idx": "idx"}),
+                        test_dd2[['y_index', 'y_idx']].rename(columns={"y_index": "index", "y_idx": "idx"})]).drop_duplicates()
+
+    drugs = pd.concat([test_dd1[['x_index', 'y_idx']].rename(columns={"x_index": "index", "y_idx": "idx"}),
+                    test_dd2[['x_index', 'x_idx']].rename(columns={"x_index": "index", "x_idx": "idx"})]).drop_duplicates()
+    
+    print(drugs.value_counts(['index', 'idx']).sort_values(ascending=False))
+    print(diseases.value_counts(['index', 'idx']).sort_values(ascending=False))
+    
+    pos = {}
+    neg = {}
+
+    pos[('drug', 'contraindication', 'disease')] = get_probs(rel="contraindication", type="pos", rev=0)
+    pos[('drug', 'indication', 'disease')] = get_probs(rel="indication", type="pos", rev=0)
+    pos[('drug', 'off-label use', 'disease')] = get_probs(rel="off-label use", type="pos", rev=0)
+    pos[('disease', 'rev_contraindication', 'drug')] = get_probs(rel="contraindication", type="pos", rev=1)
+    pos[('disease', 'rev_indication', 'drug')] = get_probs(rel="indication", type="pos", rev=1)
+    pos[('disease', 'rev_off-label use', 'drug')] = get_probs(rel="off-label use", type="pos", rev=1)
+
+
+    neg[('drug', 'contraindication', 'disease')] = get_probs(rel="contraindication", type="neg", rev=0)
+    neg[('drug', 'indication', 'disease')] = get_probs(rel="indication", type="neg", rev=0)
+    neg[('drug', 'off-label use', 'disease')] = get_probs(rel="off-label use", type="neg", rev=0)
+    neg[('disease', 'rev_contraindication', 'drug')] = get_probs(rel="contraindication", type="neg", rev=1)
+    neg[('disease', 'rev_indication', 'drug')] = get_probs(rel="indication", type="neg", rev=1)
+    neg[('disease', 'rev_off-label use', 'drug')] = get_probs(rel="off-label use", type="neg", rev=1)
+    mydict = {'pos' : pos, 'neg': neg}
+    return mydict
         
 if __name__ == "__main__":
     args, vars = util.parse_args()
@@ -173,13 +236,24 @@ if __name__ == "__main__":
     logger.warning("Starting link prediction")
     pred, target, mask = get_prediction(cfg, solver, relation_vocab)
     df = pred_to_dataframe(pred, _dataset, entity_vocab, relation_vocab)
-    df = df.loc[df.reverse==1]
-    df = df.loc[df.pred_node_type==4]
+    df = df.drop_duplicates()
     df['query_relation'] = df['query_relation'].str.split(" \(").str[0]
     #df.to_csv(os.path.join(myworkingdir, mymodel + "_predictions_txgnn.csv"), index=False, sep="\t")
     
     logger.warning("Link prediction done")
-    logger.warning("Format preds into right dictionary format for TxGNN evaluation")
+    
+    logger.warning("Format preds into right dictionary format for TxGNN evaluation - AUPRC")
+    mask = (df.reverse == 0)
+    df.loc[mask, 'tomatch'] = (df['short_x'].astype(str) + '_' +  df['short_y'].astype(str))
+    mask = (df.reverse == 1)
+    df.loc[mask, 'tomatch'] = (df['short_y'].astype(str) + '_' +  df['short_x'].astype(str))
+    # get preds in dict format
+    mydict = get_auprc_oneonone(myworkingdir, cfg, df)
+    with open(os.path.join(myworkingdir, "g_pos_neg_nbfnet.pickle"), 'wb') as handle: pickle.dump(mydict, handle, protocol=pickle.HIGHEST_PROTOCOL) 
+    
+    logger.warning("Format preds into right dictionary format for TxGNN evaluation - Recall@k")
+    df = df.loc[df.reverse==1]
+    df = df.loc[df.pred_node_type==4]
     ##################
     # format node info
     nodes = pd.read_csv(os.path.join(cfg.dataset.path, "entity_all_info.txt"), sep="\t")

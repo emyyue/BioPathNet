c84a4cf5bafee0134cd5e49f79213a706ccc8951
emyyue <emyyue>
2024-01-25 16:48:07 +0100

    add quick_eval.py to evaluate on the test set with given model checkpoint

---
+++ script/quick_eval.py
@@ -0,0 +1,105 @@
+import os
+import sys
+import pprint
+
+import torch
+
+from torchdrug import core
+from torchdrug.utils import comm
+from torch.utils import data as torch_data
+from torchdrug import data, core, utils
+import pandas as pd
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+from nbfnet import dataset, layer, model, task, util#, reasoning_mod
+import numpy as np
+
+
+def solver_load(checkpoint, load_optimizer=True):
+
+    if comm.get_rank() == 0:
+        logger.warning("Load checkpoint from %s" % checkpoint)
+    checkpoint = os.path.expanduser(checkpoint)
+    state = torch.load(checkpoint, map_location=solver.device)
+    # some issues with loading back the fact_graph and graph
+    # remove
+    state["model"].pop("fact_graph")
+    state["model"].pop("graph")
+    state["model"].pop("undirected_fact_graph")
+    # load without
+    solver.model.load_state_dict(state["model"], strict=False)
+
+
+    if load_optimizer:
+        solver.optimizer.load_state_dict(state["optimizer"])
+        for state in solver.optimizer.state.values():
+            for k, v in state.items():
+                if isinstance(v, torch.Tensor):
+                    state[k] = v.to(solver.device)
+
+    comm.synchronize()
+
+
+def build_solver(cfg):
+    cfg.task.model.num_relation = _dataset.num_relation
+    _task = core.Configurable.load_config_dict(cfg.task)
+    cfg.optimizer.params = _task.parameters()
+    optimizer = core.Configurable.load_config_dict(cfg.optimizer)
+    if "scheduler" in cfg:
+        cfg.scheduler.optimizer = optimizer
+        scheduler = core.Configurable.load_config_dict(cfg.scheduler)
+    else:
+        scheduler = None
+    return core.Engine(_task, train_set, valid_set, test_set, optimizer, scheduler, **cfg.engine)
+
+def load_vocab(dataset):
+    entity_mapping = {}
+    with open(vocab_file, "r") as fin:
+        for line in fin:
+            k, v = line.strip().split("\t")
+            entity_mapping[k] = v
+    entity_vocab = [entity_mapping[t] for t in dataset.entity_vocab]
+    relation_vocab = ["%s (%d)" % (t[t.rfind("/") + 1:].replace("_", " "), i)
+                      for i, t in enumerate(dataset.relation_vocab)]
+    
+    return entity_vocab, relation_vocab
+
+def test(cfg, solver):
+    solver.model.split = "valid"
+    solver.evaluate("valid")
+    solver.model.split = "test"
+    solver.evaluate("test")
+        
+if __name__ == "__main__":
+    args, vars = util.parse_args()
+    cfg = util.load_config(args.config, context=vars)
+    working_dir = util.create_working_directory(cfg)
+    print(working_dir)
+    vocab_file = os.path.join(os.path.dirname(__file__), cfg.dataset.path, "entity_names.txt")
+    vocab_file = os.path.abspath(vocab_file)
+
+    torch.manual_seed(args.seed + comm.get_rank())
+
+    logger = util.get_root_logger()
+    logger.warning("Working directory: %s" % working_dir)
+    logger.warning("Input Seed: %d" % args.seed)
+    logger.warning("Set Seed: %d" % args.seed + comm.get_rank())
+    if comm.get_rank() == 0:
+        logger.warning("Config file: %s" % args.config)
+        logger.warning(pprint.pformat(cfg))
+
+    cfg.dataset.files = ['train1.txt', 'train2.txt', 'valid.txt', 'test.txt']
+    _dataset = core.Configurable.load_config_dict(cfg.dataset)
+    train_set, valid_set, test_set = _dataset.split()
+    full_valid_set = valid_set
+    if comm.get_rank() == 0:
+        logger.warning(_dataset)
+        logger.warning("#train: %d, #valid: %d, #test: %d" % (len(train_set), len(valid_set), len(test_set)))
+
+    solver = build_solver(cfg)
+
+    if "checkpoint" in cfg:
+        solver_load(cfg.checkpoint)
+    entity_vocab, relation_vocab = load_vocab(_dataset)
+    
+    test(cfg, solver)

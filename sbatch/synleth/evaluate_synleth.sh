a718af0fab0dc56640195b4afb99a8f8b5cb5897
emyyue <emyyue>
2024-02-01 12:52:15 +0100

    update config for synleth

--- sbatch/synleth/evaluate_synleth.sh
+++ sbatch/synleth/evaluate_synleth.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 #SBATCH --job-name=nbfnet_biomed
-#SBATCH --output=./slurm_out/pred_synleth.txt
-#SBATCH --error=./slurm_out/pred_synleth.txt
+#SBATCH --output=./slurm_out/eval_synleth.txt
+#SBATCH --error=./slurm_out/eval_synleth.txt
 #SBATCH --time=2:00:00
 #SBATCH --mem=64Gb
 #SBATCH -c 4
 #SBATCH --gres=gpu:1
 #SBATCH -p gpu_p
 #SBATCH --qos=gpu_normal
-#SBATCH --constraint=a100_80gb
 
 CONDA_DIR=/home/icb/yue.hu/proj_genefun/conda-env/miniconda3
 eval "$($CONDA_DIR/bin/conda shell.bash hook)"
@@ -21,7 +20,5 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-11.8/targets/x86_64-linu
 
 cd /home/icb/yue.hu/proj_genefun/NBFNet
 
-for i in 2023-12-03-14-46-06-706375 2023-12-03-15-11-19-848826 2023-12-03-15-35-36-308520 2023-12-03-15-58-08-481286 2023-12-03-16-20-38-749801
-do	
-	python script/predict.py -c  config/knowledge_graph/synleth/synleth_trans_5layers_threshold_eval.yaml --gpus [0] --checkpoint /home/icb/yue.hu/proj_genefun/NBFNet/experiments/KnowledgeGraphCompletionBiomed/biomedical/NBFNet/$i/model_epoch_18.pth
-done
+python script/evaluate.py -c  config/knowledge_graph/synleth/synleth_trans_pred.yaml --gpus [0] --checkpoint /home/icb/yue.hu/proj_genefun/NBFNet/experiments/KnowledgeGraphCompletionBiomed/biomedical/NBFNet/2023-12-03-15-11-19-848826/model_epoch_18.pth
+

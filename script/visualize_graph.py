8587569e3060df3153b8ac506b4b6a5c66e954d1
emyyue <50441257+emyyue@users.noreply.github.com>
2024-08-01 18:23:01 +0200

    delete sbatch and not used config

--- script/visualize_graph.py
+++ script/visualize_graph.py
@@ -272,7 +272,6 @@ if __name__ == "__main__":
         logger.warning("Config file: %s" % args.config)
         logger.warning(pprint.pformat(cfg))
 
-    cfg.dataset.files = ['train1.txt', 'train2.txt', 'valid.txt', 'test_vis.txt']
     _dataset = core.Configurable.load_config_dict(cfg.dataset)
     train_set, valid_set, test_set = _dataset.split()
     full_valid_set = valid_set

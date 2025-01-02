f3d8f34ccdb5e3bf3d278f5d6924c98ffe0431d4
Emy Yue Hu <50441257+emyyue@users.noreply.github.com>
2025-01-02 16:01:23 +0100

    Graph perturbations (#6)
    * add code to perform perturbation experiments for G1 and G2
    
    * add run_mock_perturbations.sh
    
    * Update run_mock_perturbations.sh
    
    * Update run_mock_perturbations.sh
    
    * Update perturb_data.py
    
    * Update perturb_data.py
    
    * Update run_mock_perturbations.sh
    
    * Update perturb_data.py
    
    * Update perturb_data.py
    
    * Update run_mock_perturbations.sh
    
    * perturb_data: add mode "add_random_relations_freq"
    
    * run_mock_perturbations: add mode "add_random_relations_freq"
    
    ---------
    
    Co-authored-by: SvitlanaOleshko <34621407+SvitlanaOleshko@users.noreply.github.com>

---
+++ reproduce/run_mock_perturbations.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+#SBATCH --job-name=biopathnet_biomed_mock_perturbations
+#SBATCH --output=/lustre/groups/crna01/projects/synthetic_lethality/BioPathNet/slurm_out/run_mock_perturbations.txt
+#SBATCH --error=/lustre/groups/crna01/projects/synthetic_lethality/BioPathNet/slurm_out/run_mock_perturbations.err
+#SBATCH --time=01:00:00
+#SBATCH --mem=120Gb
+#SBATCH -c 4
+#SBATCH --gres=gpu:1
+#SBATCH -p gpu_p
+#SBATCH --qos=gpu_normal
+#SBATCH --constraint=a100_40gb
+
+
+CONDA_DIR=/home/icb/svitlana.oleshko/miniconda3
+eval "$($CONDA_DIR/bin/conda shell.bash hook)"
+conda activate biopathnet
+
+cd /lustre/groups/crna01/projects/synthetic_lethality/BioPathNet
+
+# Log the start time
+start_time=$(date +"%Y-%m-%d %H:%M:%S")
+echo "Script started at: $start_time"
+
+DATA_PATH=/lustre/groups/crna01/projects/synthetic_lethality/BioPathNet/data/mock
+SEED=123
+
+ARRAY=(
+       "train1:remove_top_kth_relation:1"
+       "train1:remove_top_kth_relation:2"
+       "train1:remove_top_kth_relation:3"
+       "train1:remove_top_kth_relation:4"
+       "train1:remove_top_kth_relation:5"
+       "train1:remove_random_relations:10"
+       "train1:remove_random_relations:20"
+       "train1:remove_random_relations:50"
+       "train1:remove_random_relations:80"
+       # "train1:add_random_relations:10"
+       # "train1:add_random_relations:20"
+       # "train1:add_random_relations:50"
+       # "train1:add_random_relations:80"
+       "train1:add_random_relations_freq:10"
+       "train1:add_random_relations_freq:20"
+       "train1:add_random_relations_freq:50"
+       "train1:add_random_relations_freq:80"     
+       "train1:remove_top_nodes:5"
+
+       "train2:remove_top_nodes:1"
+       "train2:remove_top_nodes:3"
+       "train2:remove_top_nodes:5"
+    
+       ## Optional
+       # "train2:remove_random_relations:10"
+       # "train2:remove_random_relations:20"
+       # "train2:remove_random_relations:50"
+       # "train2:remove_random_relations:80"
+       # "train2:add_random_relations:10"
+       # "train2:add_random_relations:20"
+       # "train2:add_random_relations:50"
+       # "train2:add_random_relations:80"
+    )
+
+for i in "${ARRAY[@]}"
+do
+  IFS=':' read -r which_graph perturbation_mode k <<< "$i"
+  
+  # Run the perturb_data.py script
+  python script/perturb_data.py --seed "$SEED" --data_path "$DATA_PATH" --which_graph "$which_graph" --perturbation_mode "$perturbation_mode" --k "$k"
+  
+  # Copy the mockdata_run.yaml file and rename it
+  new_yaml="config/mock/mockdata_run_${which_graph}_${perturbation_mode}_${k}.yaml"
+  cp config/mock/mockdata_run.yaml "$new_yaml"
+  
+  # Modify the new config YAML file based on which_graph
+  if [[ "$which_graph" == "train1" ]]; then
+    sed -i "s/files: \['train1.txt', 'train2.txt', 'valid.txt', 'test.txt'\]/files: \['train1_${perturbation_mode}_${k}.txt', 'train2.txt', 'valid.txt', 'test.txt'\]/" "$new_yaml"
+  elif [[ "$which_graph" == "train2" ]]; then
+    sed -i "s/files: \['train1.txt', 'train2.txt', 'valid.txt', 'test.txt'\]/files: \['train1.txt', 'train2_${perturbation_mode}_${k}.txt', 'valid.txt', 'test.txt'\]/" "$new_yaml"
+  fi
+  
+  # Run the run.py script with the new config YAML file
+  for j in 1234 1235 1236 1237 1238
+  do
+    echo "Seed $j"
+    python script/run.py -s $j -c "$new_yaml" --gpus [0]
+  done
+done
+  
+# Log the end time
+end_time=$(date +"%Y-%m-%d %H:%M:%S")
+echo "Script ended at: $end_time"
---
+++ script/perturb_data.py
@@ -0,0 +1,118 @@
+import os
+import pandas as pd
+import numpy as np
+import argparse
+
+def perturb_graph(data_path, graph, perturbation_mode, k=0, seed=123):
+    np.random.seed(seed)
+
+    if graph == 'train1':
+        df = pd.read_csv(os.path.join(data_path, 'train1.txt'), sep='\t', header=None)
+    elif graph == 'train2':
+        df = pd.read_csv(os.path.join(data_path, 'train2.txt'), sep='\t', header=None)
+    elif graph == 'train0':
+        train0_path = os.path.join(data_path, 'train0.txt')
+        if os.path.exists(train0_path):
+            df = pd.read_csv(train0_path, sep='\t', header=None)
+        else:
+            df1 = pd.read_csv(os.path.join(data_path, 'train1.txt'), sep='\t', header=None)
+            df2 = pd.read_csv(os.path.join(data_path, 'train2.txt'), sep='\t', header=None)
+            df = pd.concat([df1, df2], ignore_index=True)
+    else:
+        raise ValueError("Undefined graph. Possible options are: train0, train1, train2.")
+    
+
+    if perturbation_mode == 'remove_top_relations':
+        relations_summary = df[1].value_counts()
+        top_n_rels = relations_summary.nlargest(k).index
+        df_perturbed = df[~df[1].isin(top_n_rels)]
+
+    elif perturbation_mode == 'remove_top_kth_relation':
+        relations_summary = df[1].value_counts()
+        top_kth_rel = relations_summary.nlargest(k).index[-1]
+        df_perturbed = df[df[1] != top_kth_rel]
+    
+    elif perturbation_mode == 'remove_random_relations':
+        n_rm = round(len(df) * k / 100)
+        df_perturbed = df.sample(len(df) - n_rm)
+    
+    elif perturbation_mode == 'add_random_relations':
+        n_add = round(len(df) * k / 100)
+        nodes_1 = df[0].unique()
+        relations = df[1].unique()
+        nodes_2 = df[2].unique()
+
+        existing_edges = set()
+        for row in df.itertuples(index=False):
+            existing_edges.add((row[0], row[1], row[2]))
+            existing_edges.add((row[2], row[1], row[0]))
+
+        sampled_edges = []
+        while len(sampled_edges) < n_add:
+            new_edge = (np.random.choice(nodes_1), np.random.choice(relations), np.random.choice(nodes_2))
+            if new_edge not in existing_edges and (new_edge[2], new_edge[1], new_edge[0]) not in existing_edges:
+                sampled_edges.append(new_edge)
+                existing_edges.add(new_edge)  
+                existing_edges.add((new_edge[2], new_edge[1], new_edge[0]))
+        
+        new_edges_df = pd.DataFrame(sampled_edges, columns=[0, 1, 2])
+        df_perturbed = pd.concat([df, new_edges_df]).drop_duplicates()
+
+    elif perturbation_mode == 'add_random_relations_freq':
+        n_add = round(len(df) * k / 100)
+        
+        nodes_1 = df[0].unique()
+        relations = df[1].unique()
+        nodes_2 = df[2].unique()
+
+        relation_counts = df[1].value_counts()
+        relation_weights = relation_counts / relation_counts.sum()
+
+        existing_edges = set()
+        for row in df.itertuples(index=False):
+            existing_edges.add((row[0], row[1], row[2]))
+            existing_edges.add((row[2], row[1], row[0]))
+
+        sampled_edges = []
+        while len(sampled_edges) < n_add:
+            new_edge = (
+                np.random.choice(nodes_1), 
+                np.random.choice(relations, p=relation_weights.loc[relations].values), 
+                np.random.choice(nodes_2))
+            if new_edge not in existing_edges and (new_edge[2], new_edge[1], new_edge[0]) not in existing_edges:
+                sampled_edges.append(new_edge)
+                existing_edges.add(new_edge)  
+                existing_edges.add((new_edge[2], new_edge[1], new_edge[0]))
+        
+        new_edges_df = pd.DataFrame(sampled_edges, columns=[0, 1, 2])
+        df_perturbed = pd.concat([df, new_edges_df]).drop_duplicates()
+    
+    elif perturbation_mode == 'remove_top_nodes':
+        nodes_summary = pd.Series(df[0].tolist() + df[2].tolist()).value_counts()
+        top_p_nodes = nodes_summary.nlargest(round(len(nodes_summary) * k / 100)).index
+        df_perturbed = df[~df[0].isin(top_p_nodes) & ~df[2].isin(top_p_nodes)]
+
+    else:
+        raise ValueError("Undefined perturbation mode. Possible modes are: remove_top_relations, remove_top_kth_relation, remove_random_relations, add_random_relations, remove_top_nodes.")
+    
+    output_path = os.path.join(data_path, f'{graph}_{perturbation_mode}_{k}.txt')
+
+    df_perturbed.to_csv(output_path, sep='\t', index=False, header=False)
+    
+    return df_perturbed
+
+
+def input_arguments():
+    parser = argparse.ArgumentParser(description='Perturb factual graph')
+    parser.add_argument('--seed', type=int, default=123, help='Random state')
+    parser.add_argument('--which_graph', type=str, default="train1", help='Graph to perform perturbations on')
+    parser.add_argument('--data_path', type=str, default="/lustre/groups/crna01/projects/synthetic_lethality", help='Directory with input data')
+    parser.add_argument('--perturbation_mode', type=str, default="remove_top_relations", help='Perturbation mode')
+    parser.add_argument('--k', type=int, default=0, help='Percentage (number) of edges or node types or relation types to remove or add')
+    
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = input_arguments()
+    perturbed_data = perturb_graph(data_path=args.data_path, graph=args.which_graph, perturbation_mode=args.perturbation_mode, k=args.k, seed=args.seed)

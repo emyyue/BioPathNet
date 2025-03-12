73b85e9d52b1921754f6d03b911f3c41aafd5c17
emyyue <50441257+emyyue@users.noreply.github.com>
2025-03-12 12:22:44 +0100

    biopathnet/dataset/biomedicalInductive: correct triplet subsetting error

--- biopathnet/dataset.py
+++ biopathnet/dataset.py
@@ -595,7 +595,7 @@ class biomedicalInductive(data.KnowledgeGraphDataset):
         self.graph = self.train_graph
         # triplets: train2 + valid + test
         if self.include_factgraph:
-            self.triplets = torch.tensor(triplets[num_samples[0]:sum(num_samples[1:3])] # train2 + valid
+            self.triplets = torch.tensor(triplets[num_samples[0]:sum(num_samples[0:3])] # train2 + valid
                                          + triplets[sum(num_samples[:-1]):])
             self.num_samples = num_samples[1:3] + num_samples[4:]
         else:
